55b960df175dae0082bc7ef618f8428345d426ea
Arib Ansari <ariibansari@gmail.com>
2023-07-28 22:48:08 +0530

    hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,50 @@ const Home = () => {
     return (
         <section id="home">
             <div id="hero" className='full-height flex-centered flex-column'>
-                <h1>Madni Ansari</h1>
-                <h2>General Secretary Candidate</h2>
-                <p>Website Under Development...</p>
+                <div className='hero-content'>
+                    <div className='reverse-flex container'>
+                        <div className='calender-container'>
+                            <div className='calender'>
+                                <div className='date'>
+                                    <span>Aug</span>
+                                    <div>
+                                        2<span>nd</span>
+                                    </div>
+                                </div>
+                                <div className='date active'>
+                                    <span>Aug</span>
+                                    <div>
+                                        3<span>rd</span>
+                                    </div>
+                                </div>
+                                <div className='date'>
+                                    <span>Aug</span>
+                                    <div>
+                                        4<span>th</span>
+                                    </div>
+                                </div>
+                                <div className='date'>
+                                    <span>Aug</span>
+                                    <div>
+                                        5<span>th</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h1>Madni Ansari</h1>
+                        <h2>
+                            General Seceratary Candidate,
+                            <br />
+                            Rizvi College of Engineering
+                        </h2>
+                        <button className='button button-cta'>Vote for Me</button>
+                    </div>
+                </div>
+                <div className='hero-overlay'></div>
+
+            </div>
+            <div className='full-height flex-centered flex-column'>
+
             </div>
         </section>
     )
