31a386c94027908b230dbf9bd076ab22cceede97
Arib Ansari <ariibansari@gmail.com>
2023-07-29 01:09:40 +0530

    why me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
                             <br />
                             Rizvi College of Engineering
                         </h3>
-                        <a className='button button-cta' target='_blank' href="https://wa.me/+918104764605?text=I'm%20impressed%20by%20your%20qualifications%20and%20vision%20for%20our%20college.%20You%20have%20my%20vote%20for%20General%20Secretary,%20count%20me%20in.%20Best%20of%20luck!">
+                        <a className='button button-cta' target='_blank' rel="noreferrer" href="https://wa.me/+918104764605?text=I'm%20impressed%20by%20your%20qualifications%20and%20vision%20for%20our%20college.%20You%20have%20my%20vote%20for%20General%20Secretary,%20count%20me%20in.%20Best%20of%20luck!">
                             Show Your Support
                         </a>
                     </div>
