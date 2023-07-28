fc4106188c1fe02267bdb62041888583789491d6
Arib Ansari <ariibansari@gmail.com>
2023-07-28 23:23:04 +0530

    whatsapp cta

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,7 +41,7 @@ const Home = () => {
                             <br />
                             Rizvi College of Engineering
                         </h2>
-                        <button className='button button-cta'>Vote for Me</button>
+                        <a className='button button-cta' href="https://wa.me/+918104764605?text=I'm%20impressed%20by%20your%20qualifications%20and%20vision%20for%20our%20college.%20You%20have%20my%20vote%20for%20General%20Secretary,%20count%20me%20in.%20Best%20of%20luck!">Vote for Me</a>
                     </div>
                 </div>
                 <div className='hero-overlay'></div>
