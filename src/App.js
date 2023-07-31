72c9f4b86807e4fcee97ff1687f779d64aac12b5
Arib Ansari <ariibansari@gmail.com>
2023-08-01 03:53:31 +0530

    google analytics

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function App() {
-  
+
+  let mounted = false
+  useEffect(() => {
+    if (!mounted) {
+      mounted = true
+      ReactGA.initialize("G-TGWDM03M0F");
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
